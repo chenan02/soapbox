1914ff626d0ae7ed0a6360f54bef0bee8dc95529
Andrew Chen <chenan02@gmail.com>
2016-08-14 22:57:45 -0500

    Add channel index view

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,5 +1,5 @@
 class Channel < ActiveRecord::Base
   has_many :subscriptions
-  validates_presence_of :name
+  validates_presence_of :name, :description
   validates_uniqueness_of :name
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Channel.create(name: "Tech", description: "All things tech")
+Channel.create(name: "Politics", description: "All things politics")
+Channel.create(name: "Sports", description: "All things sports")
+Channel.create(name: "Finance", description: "All things finance")
