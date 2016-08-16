3cf818c352c019c64f2d39e5515bcaa4446a9870
Andrew Chen <chenan02@gmail.com>
2016-08-16 00:33:31 -0500

    Implement sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,12 @@
 class SessionsController < ApplicationController
-  def new
-    @session = Session.new
-  end
-
   def create
-    @user = User.where(phone_number: session_params[:phone_number])
-    unless @user || session_params[:password] == @user.password
+    @user = User.find_by(phone_number: params[:phone_number])
+    if @user && params[:password] == @user.password
       session[:user] = @user
       flash[:success] = "Signed in"
-      redirect_to channels_path
+      return redirect_to channels_path
     end
     flash[:danger] = "Invalid credentials"
     redirect_to new_session_path
-  end
-
-  private
-
-  def session_params
-    params.require(:session).permit(:phone_number, :password)
   end
 end
